6eb5d4d5681cc8fe18b84b703e5f96ef0d0de975
kirah.cox <kirahanncox@gmail.com>
2025-03-18 16:21:33 -0600

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,12 @@
+﻿using Journal;
+using System.Text.Json;
+class Program
+{
+    static void Main()
+    {
+        string path = "C:\\Users\\kirah.cox\\source\\repos\\ConsoleApp5\\ConsoleApp5\\Data.json";
+        string text = File.ReadAllText(path);
+
+        var manual = JsonSerializer.Deserialize<ZombieSurvivalJournal>(text);
+    }
+}
---
+++ ConsoleApp5/ZombieSurvivalJournal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Journal
+{
+    public class ZombieSurvivalJournal
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("entries")]
+        public List<Entry> Entries { get; set; }
+    }
+
+    public class Entry
+    {
+        [JsonPropertyName("date")]
+        public string Date { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+    }
+}
