a69b68cb93bc187fa0c751c65775f7dded956f40
kirah.cox <kirahanncox@gmail.com>
2025-03-19 16:23:43 -0600

    added a rough list of words

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -7,6 +7,34 @@ class Program
         string path = "C:\\Users\\kirah.cox\\source\\repos\\ConsoleApp5\\ConsoleApp5\\Data.json";
         string text = File.ReadAllText(path);
 
-        var manual = JsonSerializer.Deserialize<ZombieSurvivalJournal>(text);
+        ZombieSurvivalJournal manual = JsonSerializer.Deserialize<ZombieSurvivalJournal>(text);
+
+        Dictionary<string, int> keyWords = new Dictionary<string, int>();
+
+        foreach (var item in manual.Entries)
+        {
+            var words = item.Text.Split(" ");
+
+            foreach (var word in words)
+            {
+                //string.Compare(word, word.ToLower(), true);
+                //StringComparer.OrdinalIgnoreCase(word, word.ToLower());
+                //bool areEqual = string.Equals(word, word.ToLower(), StringComparison.OrdinalIgnoreCase);
+
+                if (!keyWords.ContainsKey(word))
+                {
+                    keyWords.Add(word, 1);
+                }
+                else
+                {
+                    keyWords[word]++;
+                }
+            }
+        }
+
+        foreach (var word in keyWords)
+        {
+            Console.WriteLine(word.Key + ", " + word.Value);
+        }
     }
 }
